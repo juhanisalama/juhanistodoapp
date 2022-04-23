2cd49279ff8b85098f52843858d8b8629e6d99e0
Juhani <juhani.salama@magister.fi>
2022-04-23 17:00:39 +0300

    Added CRUD methods and improved styles

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,6 +9,8 @@ import { Todo } from '../../models/todo';
 export class TodosComponent implements OnInit {
   todos?: Todo[];
 
+  inputTodo: string = '';
+
   constructor() {}
 
   ngOnInit(): void {
@@ -22,5 +24,26 @@ export class TodosComponent implements OnInit {
         completed: true,
       },
     ];
+  }
+
+  toggleDone(id: number): void {
+    this.todos?.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos?.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos?.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+
+    this.inputTodo = '';
   }
 }
